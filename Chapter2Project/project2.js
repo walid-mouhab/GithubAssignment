aa822d0d644405c7121daad98a5c0ca531a3ab56
walidmouhab <wmouhab@stumail.jccc.edu>
2025-06-15 10:20:00 -0500

    Update project2.js

--- Chapter2Project/project2.js
+++ Chapter2Project/project2.js
@@ -2,3 +2,20 @@
 let myName = "Walid Mouhab";
 let para1 = document.getElementById("p1");
 para1.textContent = myName;
+
+//////////////
+let n1 = 1000;
+let n2 = 5000;
+
+let numberSum = n1 + n2;
+document.getElementById("p2").textContent = numberSum;
+//////////////
+let numberMult = n1 * n2;
+document.getElementById("p3").textContent = numberMult;
+/////////////
+let myNameAddNum = myName + n1;
+document.getElementById("p4").textContent = myNameAddNum;
+////////////
+let myNameMultNum = myName * n1;
+document.getElementById("p5").textContent = myNameMultNum;
+////////////
