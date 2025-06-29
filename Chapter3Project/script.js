d6fcd7e8da22abb503ab6341e2d112fc92889da2
walidmouhab <wmouhab@stumail.jccc.edu>
2025-06-29 12:26:01 -0500

    Create script.js

---
+++ Chapter3Project/script.js
@@ -0,0 +1,30 @@
+// Name: Walid Mouhab
+// Date: 06/29/2025
+
+// 1. A variable to store my full name
+let myName = "Walid Mouhab";
+console.log("My Name:", myName);
+
+// 2. A variable to store my desired annual salary
+let myAnnualSalary = 61000;
+console.log("My Annual Salary:", myAnnualSalary);
+
+// 3. A variable to store my veteran status
+let myVeteranStatus = false;
+console.log("My Veteran Status:", myVeteranStatus);
+
+// 4. An array to store the names of three of my friends
+let friendsNames = ["caleb", "Amine", "Sara"];
+console.log("my friend names :", friendsNames);
+
+// 5. An array to store the desired salary of my friends
+let friendsDesiredSalaries = [120000, 44000, 55000];
+console.log(" My friends Desired Salaries:", friendsDesiredSalaries);
+
+// 6. A literal object for another friend
+let otherFriend = {
+  firstName: "adam",
+  lastName: "jones",
+  desiredAnnualSalary: 73000
+};
+console.log("Other Friend:", otherFriend);
