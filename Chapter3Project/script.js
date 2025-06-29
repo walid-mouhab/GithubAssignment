f0a3fe91fc1f75c1b596bb4d6bc3ad3e750321b5
walidmouhab <wmouhab@stumail.jccc.edu>
2025-06-29 12:32:54 -0500

    Update script.js

--- Chapter3Project/script.js
+++ Chapter3Project/script.js
@@ -19,7 +19,7 @@ console.log("4- my friend names :", friendsNames);
 
 // 5. An array to store the desired salary of my friends
 let friendsDesiredSalaries = [120000, 44000, 55000];
-console.log("5-  My friends Desired Salaries:", friendsDesiredSalaries);
+console.log("5- My friends Desired Salaries:", friendsDesiredSalaries);
 
 // 6. A literal object for another friend
 let otherFriend = {
